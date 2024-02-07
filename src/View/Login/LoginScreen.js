a4c61503c91cf02202ebc913e96ea3d5866b7341
Fellipe Nascimento Guedes <fngprogrammer@hotmail.com>
2024-02-07 18:46:43 -0300

    Feat:Design básico da tela de login finalizado

--- src/View/Login/LoginScreen.js
+++ src/View/Login/LoginScreen.js
@@ -1,26 +1,69 @@
-import { View, Text, Button, TouchableOpacity, ImageBackground, Image } from 'react-native'
-import React from 'react'
-import style from './Style/styleLogin'
+/* eslint-disable react/prop-types */
+import { View, Text, TouchableOpacity, ImageBackground, Image, TextInput } from "react-native"
+import React, { useState } from "react"
+import style from "./Style/styleLogin"
+import { Ionicons, MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons"
 
 export default function LoginScreen({ navigation }) {
-  return (
-    <View style={style.background}>
-      <View style={style.container}>
 
-        <Image style={style.image} />
+	const [nome, setNome] = useState("")
+	const [CPF, setCPF] = useState("")
+	const [dinheiro, setDinheiro] = useState("")
+	//desconsiderável só pro eslint não encher o saco por hora
+	console.log(nome + CPF + dinheiro)
 
-        <View style={style.input}>
-        </View>
+	return (
+		<ImageBackground source={require("../../../assets/fundo.jpg")} style={style.background}>
+			<View style={style.container}>
 
-        <View style={style.containerButton}>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('Drawer')}
-            style={style.buttonLogin}
-          >
-            <Text style={style.buttonTxt}>Cadastrar</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  )
+				<Image style={style.image} source={require("../../../assets/Icone.png")} />
+
+				<View style={style.input}>
+					<View style={style.containerInput}>
+						<View style={style.iconInput}>
+							<Ionicons name="person-circle-outline" size={32} color="#151515" />
+						</View>
+						<TextInput
+							style={style.txtInput}
+							placeholder="nome"
+							onChangeText={setNome}
+						/>
+					</View>
+
+					<View style={style.containerInput}>
+						<View style={style.iconInput}>
+							<MaterialCommunityIcons name="badge-account-horizontal-outline" size={24} color="black" />
+						</View>
+						<TextInput
+							style={style.txtInput}
+							placeholder="CPF"
+							onChangeText={setCPF}
+							keyboardType="numeric"
+						/>
+					</View>
+
+					<View style={style.containerInput}>
+						<View style={style.iconInput}>
+							<MaterialIcons name="attach-money" size={24} color="black" />
+						</View>
+						<TextInput
+							style={style.txtInput}
+							placeholder="dinheiro"
+							onChangeText={setDinheiro}
+							keyboardType="numeric"
+						/>
+					</View>
+				</View>
+
+				<View style={style.containerButton}>
+					<TouchableOpacity
+						onPress={() => navigation.navigate("Drawer")}
+						style={style.buttonLogin}
+					>
+						<Text style={style.buttonTxt}>Cadastrar</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</ImageBackground>
+	)
 }
