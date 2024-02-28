4f7c86dfbb190ca412dcfd3e9087131a11daa185
Fellipe Nascimento Guedes <fngprogrammer@hotmail.com>
2024-02-27 22:31:30 -0300

    feat: cria tela de mais detalhes de evento

--- src/Route/route.js
+++ src/Route/route.js
@@ -2,8 +2,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from '../View/Login/LoginScreen'
 import Evento from '../View/Evento/EventScreen'
+import DetalhesEvento from '../View/Evento/EventDetalhesScreen'
 import Estorno from '../View/Estorno/EstornoScreen'
-import { ImageBackground } from 'react-native';
 
 const Stack = createStackNavigator()
 
@@ -12,6 +12,7 @@ export default function MyStack() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Eventos" component={Evento} />
+        <Stack.Screen name="Detalhes Evento" component={DetalhesEvento} />
         <Stack.Screen name="Estorno" component={Estorno} />
       </Stack.Navigator>
   );
--- src/View/Evento/Component/Evento.js
+++ src/View/Evento/Component/Evento.js
@@ -1,27 +1,30 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import style from '../Style/styleEvento'
+import { useNavigation } from '@react-navigation/core'
 export default function Evento({evento}) {
+
+  const navigation = useNavigation()
 
   return (
     <TouchableOpacity style={style.container}>
       <View style={style.containerTitle}>
-        <Text style={style.title}>Evento</Text>
+        <Text style={style.title}>{evento.nome}</Text>
       </View>
 
       <View style={style.containerInfo}>
         <View style={style.basicDatas}>
           <View style={style.containerData}>
-            <Text style={style.status}>Em aberto</Text>
+            <Text style={style.status}>{evento.aberto?'Em aberto':'fechado'}</Text>
           </View>
 
           <View style={style.containerData}>
-            <Text style={style.date}>Data: 27/06/2024</Text>
+            <Text style={style.date}>Data: {evento.data}</Text>
           </View>
         </View>
  
         <View style={style.moreInfo}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={()=> navigation.navigate('Detalhes Evento',evento)}>
             <Text style={style.buttonMore}>Mais</Text>
           </TouchableOpacity>
         </View>
